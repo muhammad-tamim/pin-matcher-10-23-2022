a9e57f8ea0f8316ea4d77ed38eb505ee075c153b
Muhammad Tamim <mdt40234@gmail.com>
2022-10-23 22:33:27 +0600

    fix generate pin problem

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 // Find generate-btn ,generate-input and generate pin
-document.getElementById('generate-btn').addEventListener('click', function generatePin() {
+document.getElementById('generate-btn').addEventListener('click', function getPin() {
     const generateInput = document.getElementById('generate-input')
     const generatePin = Math.round(Math.random() * 10000);
     const generatePinCheck = generatePin + '';
-    if (generatePinCheck.length == 3) {
-        generatePin()
-        console.log('jhgj');
+    if (generatePinCheck.length != 4) {
+        getPin();
     }
     else {
         generateInput.value = generatePin;
